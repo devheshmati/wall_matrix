046073044b22e610a5c02ae74a593912ff334ab9
womoboy <reza2boyce@gmail.com>
2023-08-17 15:03:08 +0330

    connect to github and push everything

---
+++ script.js
@@ -0,0 +1,118 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// characters =
+//   "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//
+let textGradient = ctx.createRadialGradient(
+  canvas.width / 2,
+  canvas.height / 2,
+  100,
+  canvas.width / 2,
+  canvas.height / 2,
+  canvas.width / 2,
+);
+
+textGradient.addColorStop(0, "red");
+textGradient.addColorStop(0.4, "green");
+textGradient.addColorStop(0.6, "cyan");
+textGradient.addColorStop(1, "magenta");
+
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters =
+      "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.canvasHeight = canvasHeight;
+    this.text = "";
+  }
+  draw(context) {
+    this.text = this.characters.charAt(
+      Math.floor(Math.random() * this.characters.length),
+    );
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+      this.y = 0;
+    } else {
+      this.y += 1;
+    }
+  }
+}
+
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 20;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+    console.log(this.symbols);
+  }
+  #initialize() {
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+    }
+  }
+  resize(width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
+  }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+
+// delta time
+let lastTime = 0;
+const fps = 60;
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+const animate = (timeStamp) => {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    const grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+    grd.addColorStop(0, "rgba(0,0,0, 0.19)");
+    grd.addColorStop(1, "rgba(100, 100, 100, 0.19)");
+    ctx.fillStyle = grd;
+    ctx.textAlign = "center";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = textGradient;
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+
+  requestAnimationFrame(animate);
+};
+
+animate(0);
+
+window.addEventListener("resize", function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+
+  textGradient = ctx.createRadialGradient(
+    canvas.width / 2,
+    canvas.height / 2,
+    100,
+    canvas.width / 2,
+    canvas.height / 2,
+    canvas.width / 2,
+  );
+
+  textGradient.addColorStop(0, "red");
+  textGradient.addColorStop(0.4, "green");
+  textGradient.addColorStop(0.6, "cyan");
+  textGradient.addColorStop(1, "magenta");
+});
